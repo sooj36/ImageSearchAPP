6ed5e0a5c4d0dc6c3f0f5fe00e26128d22f21d8c
sooj36 <sooj2023@gmail.com>
2023-09-18 13:02:59 +0900

    [FEAT] : ⚙️ | Fragment.kt (first,second)

--- app/src/main/java/com/example/imagesearchapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/imagesearchapp/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.imagesearchapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.imagesearchapp.databinding.ActivityMainBinding
 import com.example.imagesearchapp.ui.adapter.ViewPagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
@@ -21,11 +22,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // viewpager,tablayout 참조
         val viewPager = binding.viewpager
         val tabLayout = binding.tablayout
 
+        // viewpager의 adapter 설정
         viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
 
+        // tablayout - viewpager 연결 , tabitem 메뉴명 설정
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = tabTitleArray[position]
         }.attach()
--- app/src/main/java/com/example/imagesearchapp/ui/fragment/FirstFragment.kt
+++ app/src/main/java/com/example/imagesearchapp/ui/fragment/FirstFragment.kt
@@ -6,28 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.imagesearchapp.R
-import com.example.imagesearchapp.databinding.ActivityMainBinding
+import com.example.imagesearchapp.databinding.FragmentFirstBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FirstFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FirstFragment : Fragment() {
 
-    private lateinit var binding: ActivityMainBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        return binding.root
+    class FirstFragment : Fragment() {
+        private lateinit var binding : FragmentFirstBinding
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            binding = FragmentFirstBinding.inflate(inflater, container, false)
+            return binding.root
+        }
+
     }
-}
