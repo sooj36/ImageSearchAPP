80037141bb10eb7deadd85975088f5dc376bd528
sooj36 <sooj2023@gmail.com>
2023-09-18 11:31:21 +0900

    [FEAT] : ⚙️ | Main.kt (TabLayout , ViewPager2 연동)

--- app/src/main/java/com/example/imagesearchapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/imagesearchapp/ui/activity/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.imagesearchapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.imagesearchapp.R
+import com.example.imagesearchapp.databinding.ActivityMainBinding
+import com.example.imagesearchapp.ui.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+
+// TabLayout , ViewPager2 연동
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private val tabTitleArray:Array<String> = arrayOf(
+        "검색 결과",
+        "내 보관함"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewPager = binding.viewpager
+        val tabLayout = binding.tablayout
+
+        viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
     }
 }
