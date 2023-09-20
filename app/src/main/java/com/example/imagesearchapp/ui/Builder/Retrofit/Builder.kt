c90111f9f02ed55a44e222287994a9230d8fea0f
sooj36 <sooj2023@gmail.com>
2023-09-20 15:32:19 +0900

    DEV

---
+++ app/src/main/java/com/example/imagesearchapp/ui/Builder/Retrofit/Builder.kt
@@ -0,0 +1,9 @@
+package com.example.imagesearchapp.ui.Builder.Retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+//interface를 사용하는 인스턴스,
+// Builder는 BASE_URL 와 Converter 를 설정
+object RetrofitInstance {
+}
---
+++ app/src/main/java/com/example/imagesearchapp/ui/config/Constants.kt
@@ -0,0 +1,14 @@
+package com.example.imagesearchapp.ui.config
+
+//상수 저장
+
+object API {
+    const val REST_API_KEY = "4fef37c0901dbba01264324ed236e61c"
+
+    const val AUTH_HEADER = "KakaoAK $REST_API_KEY"
+
+    const val KAKAO_OPEN_API_URL ="https://dapi.kakao.com/v2/search/image"
+
+    const val EMPTY_RESULT = "EMPTY_RESULT"
+
+    }
